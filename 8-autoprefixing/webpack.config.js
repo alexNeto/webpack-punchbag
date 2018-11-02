c40f69fc1242f66600d7e71b91b21339ca7d33ce
alexNeto <alex.neto3@hotmail.com>
2018-11-02 16:56:12 -0300

    autoprefixing para adicionar ou remover regra de css dependendo dos browsers alvo

---
+++ 8-autoprefixing/webpack.config.js
@@ -0,0 +1,51 @@
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+const glob = require("glob");
+
+const parts = require("./webpack.parts");
+
+const PATHS = {
+    app: path.join(__dirname, "src"),
+};
+
+const commonConfig = merge([{
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack demo",
+        }),
+    ]
+}, ]);
+
+const productionConfig = merge([
+    parts.extractCSS({
+        use: ["css-loader", parts.autoprefix()],
+    }),
+    // parts.purifyCSS({
+    //     paths: glob.sync(`${PATHS.app}/**/.js`, {
+    //         nodir: true,
+    //     }),
+    // }),
+]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+    parts.loadCSS(),
+]);
+
+module.exports = mode => {
+    if (mode === "production") {
+        return merge(commonConfig, productionConfig, {
+            mode
+        });
+    }
+
+    return merge(commonConfig, developmentConfig, {
+        mode
+    });
+};

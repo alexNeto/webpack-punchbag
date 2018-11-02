2189c47aea411e2067a7c0fcaf9977ba34fbcad4
alexNeto <alex.neto3@hotmail.com>
2018-11-02 11:51:58 -0300

    separando arquivos de configuração

--- 3-watch-mode/webpack.config.js
+++ 3-watch-mode/webpack.config.js
@@ -1,18 +1,21 @@
-// const HtmlWebpackPlugin = require("html-webpack-plugin")
-const MiniHtmlWebpackPlugin = require("mini-html-webpack-plugin")
-const minify = require('html-minifier').minify;
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+    devServer: {
+        watchOptions: {
+            // Delay the rebuild after the first change
+            aggregateTimeout: 300,
+
+            // Poll using interval (in ms, accepts boolean too)
+            poll: 1000,
+        }
+    },
     plugins: [
-        new MiniHtmlWebpackPlugin({
-            context: {
-                title: "webpack demo"
-            },
-            template: context =>
-                minify(MiniHtmlWebpackPlugin.defaultTemplate(context))
-        }),
-        // new HtmlWebpackPlugin({
-        //     title: "Webpack demo",
-        // }),
-    ],
+        // Ignore node_modules so CPU usage with poll
+        // Watching drops significantly.
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, "node_modules")
+        ])
+    ]
 };
